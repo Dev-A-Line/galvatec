73471659843581b10e067d54d4846d697c8dbd7c
Dev-A-Line <SeregaALain@gmail.com>
2023-05-04 05:12:48 +0300

    first commit

---
+++ lang/lang.js
@@ -0,0 +1,93 @@
+const langArr = {
+		"title": {
+					"ru": "Гальватек - Технологии и решения для автоматизации производства",
+					"en": "Galvatec - Technologies and solutions for industrial automation",
+		},
+		"desc": {
+			"ru": "Гальватек – молодая и амбициозная технологическая компания, разрабатывающая наукоемкие решения на рынке автоматизации производства. Ключевое направление - гальванические линии, оборудование и программное обеспечение для них. Среди наших клиентов - крупные предприятия и производственные объекты машиностроительной отрасли, госкомпании.",
+			"en": "Galvatek - a young and ambitious technology company developing knowledge-intensive solutions in the production automation market. Our key focus is on galvanic lines, equipment and software for them. Our customers include large companies and production facilities in the mechanical engineering industry, as well as state-owned companies."
+		},
+		"address": {
+			"ru": "123112 г. Москва, Пресненская набережная, д.12, этаж 45, комната 82, офис 2.",
+			"en": "123112 г. Moscow, Presnenskaya Naberezhnaya 12, floor 45, room 82, office 2.",
+		},	
+		"copy-title": {
+			"ru": "© АО “Гальватек” 2023",
+			"en": "© JSC “Galvatec” 2023",
+		},	
+		"hero-title": {
+					"ru": "Технологии и решения для автоматизации производства",
+					"en": "Technologies and solutions for factory automation"
+		},
+		"menu-about": {
+			"ru": "О компании",
+			"en": "About us"
+		},
+		"menu-designing": {
+			"ru": "Проектирование",
+			"en": "Designing"
+		},
+		"menu-contact": {
+			"ru": "Контакты",
+			"en": "Contact"
+		},
+		"btn-call": {
+			"ru": "Заказать проект",
+			"en": "Get in touch"
+		},
+		"btn-call2": {
+			"ru": "Заказать проект",
+			"en": "Get Started Designing"
+		},
+		"services-supplies-galvanic": {
+			"ru": "Поставки электрического оборудования для гальваники",
+			"en": "Supplies of electrical equipment for the electroplating industry"
+		},
+		"services-supplies-electric": {
+			"ru": "Поставки электрического оборудования",
+			"en": "Electrical equipment supplies"
+		},
+		"services-develop": {
+			"ru": "Разработка программного обеспечения для автоматизации",
+			"en": "Developing software for automation"
+		},
+		"services-designing": {
+			"ru": "Проектирование оборудования для сферы IoT",
+			"en": "Designing equipment for the IoT sector"
+		},
+		"form-name": {
+			"ru": "Имя",
+			"en": "Name"
+		},
+		"form-phone": {
+			"ru": "Номер",
+			"en": "Phone"
+		},
+		"submit": {
+			"ru": "Отправить",
+			"en": "Send"
+		},
+		"jobs": {
+			"ru": "Вакансии",
+			"en": "Jobs"
+		},
+		"message-form": {
+			"ru": "Оставьте заявку, мы вам поможем!",
+			"en": "Leave a request, we'll call you!"
+		},
+		"personal-info": {
+			"ru": "Согласие на обработку персональных данных",
+			"en": "Consent to the processing of personal data"
+		},
+		"thank-call": {
+			"ru": "Спасибо за заявку!",
+			"en": "Thank you for the application!"
+		},
+		"thank-message": {
+			"ru": "В ближайшее время с вами свяжется наш менеджер.",
+			"en": "Our manager will contact you shortly."
+		},
+		
+
+
+}
